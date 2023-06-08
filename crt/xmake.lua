fa8c4171612ef398400e274453f1392601117143
Arthur LAURENT <arthur.laurent4@gmail.com>
2023-06-08 17:56:35 +0200

    fix .o output directory for crt files

--- crt/xmake.lua
+++ crt/xmake.lua
@@ -3,16 +3,7 @@ add_rules("mode.debug", "mode.release")
 set_allowedplats("switch")
 set_allowedarchs("switch|aarch64")
 
-option("objectdir")
-    set_default("lib")
-    set_showmenu(true)
-option_end()
-
 target("switch-support-files")
     set_kind("object")
 
     add_files("src/*.S")
-
-    add_options("objectdir")
-
-    set_objectdir("$(objectdir)")
